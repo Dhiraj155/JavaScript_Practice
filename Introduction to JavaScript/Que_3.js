6ce027ca4999c505b850a89c78f02002f332fdd5
Dhiraj155 <dhirajwani155@gmail.com>
2022-08-12 20:21:27 +0530

    Que 3

---
+++ Introduction to JavaScript/Que_3.js
@@ -0,0 +1,21 @@
+//Declaring variable
+//Using javascript variables, declare a variable named newVariable and assign it the following string, "AlmaBetter".
+
+//Example 1:
+//Input:
+//"AlmaBetter"
+
+//Output:
+//newVariable = AlmaBetter
+
+//Explanation:
+//Assigned the AlmaBetter string to the newVariable.
+
+//Constraints:
+//Restrict to the input format that is mentioned. Use whatever you have learned in live class curriculum to solve this challenge.
+
+//Ans--
+
+let newVariable;
+newVariable="Almabetter";
+console.log(newVariable);
