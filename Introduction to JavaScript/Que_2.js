063960622822b48943deec5cc204fcf82323d164
Dhiraj155 <dhirajwani155@gmail.com>
2022-08-12 20:14:30 +0530

    Que 2

---
+++ Introduction to JavaScript/Que_2.js
@@ -0,0 +1,20 @@
+//Printing Input
+//Print the input specified: input = "This is the first and only line of input string".
+
+//Example 1:
+//Input:
+//var input = "This is the first and only line of input string";
+
+//Output:
+//This is the first and only line of input string
+
+//Explanation:
+//Printed the input specified, hence, the output.
+
+//Constraints:
+//Restrict to the input format that is mentioned. Use whatever you have learned in live class curriculum to solve this challenge.
+
+//Ans--
+
+let input="This is the first and only line of input string";
+console.log(input);
