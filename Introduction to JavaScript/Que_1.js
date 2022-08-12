8cb3ef78d0bda9eb210cfa536dc9e45b030da45f
Dhiraj155 <dhirajwani155@gmail.com>
2022-08-12 20:09:30 +0530

    Que 1

---
+++ Introduction to JavaScript/Que_1.js
@@ -0,0 +1,20 @@
+//Hello World
+//Print Hello World! in the console below.
+
+//Example 1:
+//Input:
+//console.log("Hello World!");
+
+//Output:
+//Hello World!
+
+//Explanation:
+//Printed the Hello World using console.log, hence, the output.
+
+//Constraints:
+//Restrict to the input format that is mentioned. Use whatever you have learned in live class curriculum to solve this challenge.
+
+//Ans--
+
+let a="Hello World!";
+console.log(a);
