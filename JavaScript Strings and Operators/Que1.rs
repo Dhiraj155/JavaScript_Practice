ba2fe05fb96870442977e01fe38ab7abb342f19e
Dhiraj155 <dhirajwani155@gmail.com>
2022-08-16 20:53:12 +0530

    Que 1

---
+++ JavaScript Strings and Operators/Que1.rs
@@ -0,0 +1,39 @@
+//Javascript Operators
+//Javascript Operators Assign two numeric variables a and b Initialise values as 5 and 10 respectively and console log in same order the following:
+
+//Create a variable add and assign it the sum of a and b.
+//Create a variable sub and assign it result of b subtracted from a.
+//Create a variable mul and assign it the product of a and b.
+//Create a variable div and assign it the result of of a divided by b.
+//Create a variable inc and assign it the preincremented value of a.
+//Create a variable dec and assign it the predecremented value of b.
+//Example 1:
+//Input:
+//var a = 5 var b = 10
+
+//Output:
+//15 5 50 0.5 6 9
+
+//Explanation:
+//We have operate the given operations in ascending order and printed the result at every instance, Hence, the output.
+
+//Constraints:
+//Restrict to the input format that is mentioned. Use whatever you have learned in live class curriculum to solve this challenge.
+
+//ans--
+
+var a= 5
+var b = 10
+add = a+b
+sub = b-a
+mul = a*b
+div = a/b
+inc = ++a
+dec = --b
+
+console.log(add);
+console.log(sub);
+console.log(mul);
+console.log(div);
+console.log(inc);
+console.log(dec);
