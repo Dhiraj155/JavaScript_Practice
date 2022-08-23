d2ef6eac33d1e2a90995852fdb4d0ae3191b4dc5
Dhiraj155 <dhirajwani155@gmail.com>
2022-08-23 18:18:27 +0530

    Que 3

---
+++ JavaScript Object and JSON/Que3.js
@@ -0,0 +1,22 @@
+//Destructuring Syntax
+//Updating Nested Objects
+
+//Use the object given here as the database object.
+//Use the destructuring syntax to store only the values of name, jobTitle and work properties to respective variables
+//console log all 3 variables together
+//Example 1:
+//Input:
+//const userActivity3 = { name: "Rahul", jobTitle: "JS Instructor@almabetter.com", work: "rahul@microsoft.com”, region: "Hyderabad" }
+
+//Output:
+//Rahul JS Instructor@almabetter.com rahul@microsoft.com
+
+//Explanation:
+//Perform desturcturing using destructuring syntax and print the required properties, hence the output.
+
+//Constraints:
+//Restrict to input format only and update details as per expected output to get your code evaluated.
+
+//ans--
+
+
