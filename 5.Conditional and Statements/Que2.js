7881579c12e515b96513158eb04af09ec3c3eac8
Dhiraj155 <dhirajwani155@gmail.com>
2022-08-24 18:19:55 +0530

    Que 2

---
+++ 5.Conditional and Statements/Que2.js
@@ -0,0 +1,31 @@
+//Even or Odd
+//Write a JavaScript program that checks if input number is even or odd by using “%” operator in JavaScript.
+
+//Example 1:
+//Input:
+//var num3 = 17.5 var num4 = 42
+
+//Output:
+//17.5 is odd 42 is even
+
+//Explanation:
+//Out of the inputed numbers 17.5 and 42 as 17.5 is odd and 42 is even, hence the output.
+
+//Constraints:
+//Restict to variables that are given in input format to get your code evaluated.
+
+//ans---
+
+var num3 = 17.5
+var num4 = 42
+if(num3%2===0){
+    console.log(num3+" is even")
+}else {
+    console.log(num3+" is odd")
+}
+
+if(num4%2===0){
+    console.log(num4+" is even")
+}else {
+    console.log(num4+" is odd")
+}
