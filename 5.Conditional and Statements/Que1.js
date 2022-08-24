be3a178bc27c99f1aac34283a09bb6a24ef870c5
Dhiraj155 <dhirajwani155@gmail.com>
2022-08-24 17:38:48 +0530

    Que 1

---
+++ 5.Conditional and Statements/Que1.js
@@ -0,0 +1,27 @@
+//Display Large Integer
+//Write a JavaScript program that accept two integers and display the larger.
+
+//Example 1:
+//Input:
+//var num1 = 5 var num2 = 10
+
+//Output:
+//The larger of 5 and 10 is 10.
+
+//Explanation:
+//The larger integer out of 5 and 10 is 10, hence the output.
+
+//Constraints:
+//Restict to variables that are given in input format to get your code evaluated.
+
+//ans--
+
+
+let num1 = 5;
+let num2 = 10;
+
+if(num1 > num2){
+    console.log(`The larger of the ${num1} and ${num2} is ${num1}.`)
+} else {
+    console.log(`The larger of the ${num1} and ${num2} is ${num2}.`)
+}
